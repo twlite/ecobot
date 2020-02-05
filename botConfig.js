ae8ff7b2edf079f39a636740902acca6aca1925b
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-02-05 14:16:52 +0545

    Create botConfig.js

---
+++ botConfig.js
@@ -0,0 +1,22 @@
+module.exports = {
+    token: "Your_Bot_Token",
+    prefix: ":?",
+    admins: [
+        "Your_ID"
+    ],
+    games: [
+        {
+            type: "PLAYING",
+            name: "with {users} users"
+        },
+        {
+            type: "LISTENING",
+            name: "{prefix}help"
+        },
+        {
+            type: "WATCHING",
+            name: "{servers} servers"
+        }
+    ],
+    debug: true
+};
