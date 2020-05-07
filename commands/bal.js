025c11c154fc5735f3d960adfa97e8d15782142e
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-05-07 07:44:22 +0545

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -2,11 +2,12 @@ const { RichEmbed } = require("discord.js");
 
 module.exports.execute = async (client, message, args) => {
     let user = message.mentions.users.first() || message.author;
-    let userBalance = await client.eco.fetchMoney(user.id);
+    let userBalance = client.eco.fetchMoney(user.id);
     const embed = new RichEmbed()
         .setTitle(`Balance`)
         .addField(`User`, `<@${userBalance.user}>`)
         .addField(`Balance`, `${userBalance.amount} 💸`)
+        .addField(`Position`, userBalance.position)
         .setColor("RANDOM")
         .setThumbnail(user.displayAvatarURL)
         .setTimestamp();
