f960dc1ce21d892e97fae7890a9ef22b0ad22e33
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-05-07 07:45:54 +0545

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -6,7 +6,7 @@ module.exports.execute = async (client, message, args) => {
         "Peter"
     ];
     let amount = Math.floor(Math.random() * 50) + 10;
-    let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: true });
+    let beg = client.eco.beg(client.ecoAddUser, amount, { canLose: true });
     if (beg.onCooldown) return message.reply(`Begon Thot! Come back after ${beg.time.seconds} seconds.`);
     if (beg.lost) return message.channel.send(`**${users[Math.floor(Math.random() * users.length)]}:** Begon Thot! Try again later.`);
     else return message.reply(`**${users[Math.floor(Math.random() * users.length)]}** donated you **${beg.amount}** 💸. Now you have **${beg.after}** 💸.`);
