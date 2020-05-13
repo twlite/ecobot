086804f1466a12bab799895bbdcabd4260fcd0e2
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-05-13 17:46:42 +0545

    Add: shop command

---
+++ commands/shop.js
@@ -0,0 +1,23 @@
+const { RichEmbed } = require("discord.js");
+
+module.exports.execute = async (client, message, args) => {
+  let items = Object.keys(client.shop);
+  let content = "";
+  
+  for (var i in items) {
+    content += `${items[i]} - :dollar: ${client.shop[items[i]].prize}\n`
+  }
+  
+  let embed = new RichEmbed()
+  .setTitle("Store")
+  .setDescription(content)
+  .setColor("BLURPLE")
+  .setFooter("Do :?buy <item> to purchase the item.")
+  return message.channel.send(embed);
+};
+
+module.exports.help = {
+  name: "shop",
+  aliases: [],
+  usage: `shop`
+};
