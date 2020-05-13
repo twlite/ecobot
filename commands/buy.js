1dc6cf13f85bc3d60cb866d4da9fad5646f1525e
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-05-13 19:50:21 +0545

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -7,17 +7,17 @@ module.exports.execute = async (client, message, args) => {
   if (!item) return message.channel.send("What are you trying to buy?");
   let hasItem = client.shop[item.toLowerCase()];
   if (!hasItem || hasItem == undefined) return message.reply("That item doesnt exists lol");
-  let isBalanceEnough = (userBalance.amount >= hasItem.prize);
+  let isBalanceEnough = (userBalance.amount >= hasItem.cost);
   if (!isBalanceEnough) return message.reply("Your balance is insufficient. You need :dollar: "+hasItem.prize+" to buy this item.");
-  let buy = client.eco.removeMoney(message.author.id, hasItem.prize);
+  let buy = client.eco.removeMoney(message.author.id, hasItem.cost);
   
   let itemStruct = {
     name: item.toLowerCase(),
-    prize: hasItem.prize
+    prize: hasItem.cost
   };
   
   client.db.push(`items_${message.author.id}`, itemStruct);
-  return message.channel.send(`You purchased **${item}** for **:dollar: ${hasItem.prize}**.`);
+  return message.channel.send(`You purchased **${item}** for **:dollar: ${hasItem.cost}**.`);
 };
 
 module.exports.help = {
